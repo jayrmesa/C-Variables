2e3af9d29bfaacd93246502b482284768e17288d
Jayr Mesa <jayrmesa@gmail.com>
2023-10-16 14:45:50 -0600

    Change the type to var

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -5,12 +5,12 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Mosh";
-            bool isWorking = false;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = false;
 
             Console.WriteLine(number);
             Console.WriteLine(count);
